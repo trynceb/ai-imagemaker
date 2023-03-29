9dc09f3962eb283ae5844d1efd6dca0cc7a5208f
Trynce Brandt <tjbrandt2013@gmail.com>
2023-03-29 08:40:22 -0400

    buildout create post page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -44,12 +44,14 @@ const Home = () => {
           <div className="grid lg:grid-cols-4 sm:grid-cols-3 sx:gid-cols-2 grid-cols-1 gap-3">
             {searchText ? (
               <RenderCards
-                data="searchedResults"
+                data={[]}
                 title="No search results found"
               />
             ) : (
-              <>
-              </>
+              <RenderCards
+                data={[]}
+                title="No posts found"
+              />
             )}
           </div>
           </>

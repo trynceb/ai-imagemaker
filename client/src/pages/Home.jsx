64e09b2776ce41f32cd6a63ce2549c7a585e4fa8
Trynce Brandt <tjbrandt2013@gmail.com>
2023-04-04 20:37:39 -0400

    add search bar

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -16,7 +16,10 @@ const RenderCards = ({ data, title }) => {
 const Home = () => {
   const [isLoading, setIsLoading] = useState(false)
   const [allPosts, setAllPosts] = useState(null)
+
   const [searchText, setSearchText] = useState("")
+  const [searchResults, setSearchedResults] = useState(null)
+  const [searchTimeout, setSearchTimeout] = useState(null)
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -45,6 +48,18 @@ const Home = () => {
     fetchPosts()
   }, [])
 
+  const handleSearchChange = (e) => {
+    clearTimeout(searchTimeout);
+    setSearchText(e.target.value);
+
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResult = allPosts.filter((item) => item.name.toLowerCase().includes(searchText.toLowerCase()) || item.prompt.toLowerCase().includes(searchText.toLowerCase()))
+        setSearchedResults(searchResult)
+      }, 500),
+    )
+  }
+
   return (
     <section className="max-w-7x1 mx-auto">
       <div>
@@ -53,7 +68,14 @@ const Home = () => {
       </div>
 
       <div className="mt-16">
-        <FormField />
+        <FormField 
+          LabelName="Search posts"
+          type="text"
+          name="text"
+          placeholder="Search posts"
+          value={searchText}
+          handleChange={handleSearchChange}
+        />
       </div>
 
       <div className="mt-10">
@@ -71,7 +93,7 @@ const Home = () => {
           <div className="grid lg:grid-cols-4 sm:grid-cols-3 sx:gid-cols-2 grid-cols-1 gap-3">
             {searchText ? (
               <RenderCards
-                data={[]}
+                data={searchResults}
                 title="No search results found"
               />
             ) : (
